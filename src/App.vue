9a8b947d433aa43a4b50310de56bb0dbb7456f2d
Kyle Wagner <kylemwagner83@gmail.com>
2022-08-29 15:53:58 -0600

    Computed property

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <script>
-  // give each todo a unique id
   let id = 0
   
   export default {
     data() {
       return {
         newTodo: '',
+        hideCompleted: false,
         todos: [
-          { id: id++, text: 'Learn HTML' },
-          { id: id++, text: 'Learn JavaScript' },
-          { id: id++, text: 'Learn Vue' }
+          { id: id++, text: 'Learn HTML', done: true },
+          { id: id++, text: 'Learn JavaScript', done: true },
+          { id: id++, text: 'Learn Vue', done: false }
         ]
+      }
+    },
+    computed: {
+      filteredTodos() {
+        return this.hideCompleted
+          ? this.todos.filter((t) => !t.done)
+          : this.todos
       }
     },
     methods: {
       addTodo() {
-        this.todos.push({id: id++, text: this.newTodo})
+        this.todos.push({ id: id++, text: this.newTodo, done: false })
         this.newTodo = ''
       },
       removeTodo(todo) {
@@ -27,13 +34,23 @@
   
   <template>
     <form @submit.prevent="addTodo">
-      <input v-model="newTodo">
-      <button>Add Todo</button>    
+      <input v-model="newTodo" />
+      <button>Add Todo</button>
     </form>
     <ul>
-      <li v-for="todo in todos" :key="todo.id">
-        {{ todo.text }}
+      <li v-for="todo in filteredTodos" :key="todo.id">
+        <input type="checkbox" v-model="todo.done">
+        <span :class="{ done: todo.done }">{{ todo.text }}</span>
         <button @click="removeTodo(todo)">X</button>
       </li>
     </ul>
+    <button @click="hideCompleted = !hideCompleted">
+      {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+    </button>
   </template>
+  
+  <style>
+  .done {
+    text-decoration: line-through;
+  }
+  </style>
