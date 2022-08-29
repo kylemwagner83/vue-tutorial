d849447eb8f8ec87b8591451c079fb4df6a524ee
Kyle Wagner <kylemwagner83@gmail.com>
2022-08-29 14:41:55 -0600

    Form binding

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,23 @@
   export default {
     data() {
       return {
-        count: 0
+        text: ''
       }
     },
-    methods: {
-      increment() {
-        this.count++
-      }
-    }
+    // methods: {
+    //   onInput(e) {
+    //     this.text = e.target.value
+    //   }
+    // }
   }
   </script>
   
   <template>
-    <!-- make this button work -->
-    <button @click="increment">count is: {{ count }}</button>
+    <!-- <input :value="text" @input="onInput" placeholder="Type here"> -->
+
+    <input v-model="text" placeholder="Type here">
+    <!-- v-model automatically syncs the input value with the bound state,
+      so the event handler isn't required -->
+      
+    <p>{{ text }}</p>
   </template>
