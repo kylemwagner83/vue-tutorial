e1d97a8edb06b609401a9c5c73e177325e474c36
Kyle Wagner <kylemwagner83@gmail.com>
2022-08-29 15:06:54 -0600

    Render list with for loop

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <script>
+  // give each todo a unique id
+  let id = 0
+  
   export default {
     data() {
       return {
-        awesome: true
+        newTodo: '',
+        todos: [
+          { id: id++, text: 'Learn HTML' },
+          { id: id++, text: 'Learn JavaScript' },
+          { id: id++, text: 'Learn Vue' }
+        ]
       }
     },
     methods: {
-      toggle() {
-        this.awesome = !this.awesome
+      addTodo() {
+        this.todos.push({id: id++, text: this.newTodo})
+        this.newTodo = ''
+      },
+      removeTodo(todo) {
+        this.todos = this.todos.filter((t) => t !== todo)
       }
     }
   }
   </script>
   
   <template>
-    <button @click="toggle">toggle</button>
-    <h1 v-if="awesome">Vue is awesome!</h1>
-    <h1 v-else>Oh no 😢</h1>
+    <form @submit.prevent="addTodo">
+      <input v-model="newTodo">
+      <button>Add Todo</button>    
+    </form>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.text }}
+        <button @click="removeTodo(todo)">X</button>
+      </li>
+    </ul>
   </template>
