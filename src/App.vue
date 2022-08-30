e182b627aae4465c8723cae6ae818b066e5b415b
Kyle Wagner <kylemwagner83@gmail.com>
2022-08-30 09:35:09 -0600

    Watchers

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <script>
   export default {
+    data() {
+      return {
+        todoId: 1,
+        todoData: null
+      }
+    },
+    methods: {
+      async fetchData() {
+        this.todoData = null
+        const res = await fetch(
+          `https://jsonplaceholder.typicode.com/todos/${this.todoId}`
+        )
+        this.todoData = await res.json()
+      }
+    },
+    watch: {
+      todoId(newID) {
+        this.fetchData()
+      }
+    },
     mounted() {
-      this.$refs.p.textContent = 'mounted!'
+      this.fetchData()
     }
   }
   </script>
   
   <template>
-    <p ref="p">hello</p>
+    <p>Todo id: {{ todoId }}</p>
+    <button @click="todoId++">Fetch next todo</button>
+    <p v-if="!todoData">Loading...</p>
+    <pre v-else>{{ todoData }}</pre>
   </template>
