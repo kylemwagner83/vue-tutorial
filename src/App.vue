e304506b210494a931e863a9a3e049aa4fc9e8c5
Kyle Wagner <kylemwagner83@gmail.com>
2022-09-01 10:12:29 -0600

    Components

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,14 @@
+
 <script>
-  export default {
-    data() {
-      return {
-        todoId: 1,
-        todoData: null
-      }
-    },
-    methods: {
-      async fetchData() {
-        this.todoData = null
-        const res = await fetch(
-          `https://jsonplaceholder.typicode.com/todos/${this.todoId}`
-        )
-        this.todoData = await res.json()
-      }
-    },
-    watch: {
-      todoId(newID) {
-        this.fetchData()
-      }
-    },
-    mounted() {
-      this.fetchData()
-    }
+import ChildComp from './ChildComp.vue'
+
+export default {
+  components: {
+    ChildComp
   }
-  </script>
+}
+</script>
   
-  <template>
-    <p>Todo id: {{ todoId }}</p>
-    <button @click="todoId++">Fetch next todo</button>
-    <p v-if="!todoData">Loading...</p>
-    <pre v-else>{{ todoData }}</pre>
-  </template>
+<template>
+  <ChildComp />
+</template>
